3c036240fb075779a3c350aec14fb91495a1e50f
narasimha vadla <narasimhavadla12@gmail.com>
2025-11-29 22:01:52 +0530

    Updated

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -18,7 +18,7 @@ export default function Home() {
       {/* ================= HERO SECTION ================= */}
       <section className="hero">
         <div className="intro card ">
-          <h2>Hi, I'm Narasimha. — I build reliable frontend systems.</h2>
+          <h2>Hi, I'm Narasimha. — I build reliable frontend systems..</h2>
 
           <p className="">
             I’m a backend-focused Java developer who enjoys building clean,
